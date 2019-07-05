e0616501a2c8f1748c60c86608f307632844a373
Quốc Anh Lê <anhlq.uit@gmail.com>
2019-07-05 14:52:43 +0700

    Create project

---
+++ DemoTwitter/DemoTwitterTests/DemoTwitterTests.swift
@@ -0,0 +1,34 @@
+//
+//  DemoTwitterTests.swift
+//  DemoTwitterTests
+//
+//  Created by Anh Lê on 7/5/19.
+//  Copyright © 2019 Anh Lê. All rights reserved.
+//
+
+import XCTest
+@testable import DemoTwitter
+
+class DemoTwitterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
